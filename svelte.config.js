1fa6379d62e017b6745dd1748e1c88ba7d9a0e1b
fq28 <fqdejager@gmail.com>
2024-12-16 23:12:22 +0100

    nothing

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: adapter({
-            fallback: 'index.html', // For SPA routing
-        }),
-        paths: {
-            base: '/simple-escape', // Update to match your repo name
-        },
-    },
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html' // For SPA routing
+		}),
+		paths: {
+			base: '/simple-escape' // Explicitly set to match your repo name
+		}
+	}
 };
 
 export default config;
